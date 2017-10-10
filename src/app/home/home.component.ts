1a2a36e7fbd4acf813e7f5ab2ae252a9c3f0c1d6
Raphael Freitas <dev.rapha@gmail.com>
2017-10-09 22:32:29 -0300

    class ofer + service + component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OffersService } from '../shared/offers.service';
+import { Offer } from '../shared/offer.model';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +12,20 @@ export class HomeComponent implements OnInit {
 
   constructor(private offersService: OffersService) { }
 
+  public offers: Offer[];
+
   ngOnInit() {
-    console.log(this.offersService.getOffers());
+
+    //this.offers = this.offersService.getOffers();
+
+    this.offersService.getOffersPromise()
+      .then((offers: Offer[]) => {
+        console.log(' req de 3s ')
+        this.offers = offers
+      })
+      .catch(err => console.log(err) )
+
+    console.log(this.offers);
   }
 
 }
