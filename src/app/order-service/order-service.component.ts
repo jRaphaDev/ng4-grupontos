11249061a7d395ccd7a3e7810b5d9acc17bfb745
Raphael Freitas <p000rasantos@prservicos.com.br>
2017-11-21 17:59:58 -0200

    template forms

--- src/app/order-service/order-service.component.ts
+++ src/app/order-service/order-service.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { OrderServiceService } from '../shared/order-service.service';
 import { OrderService } from '../shared/order-service.model';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-order-service',
@@ -10,75 +11,14 @@ import { OrderService } from '../shared/order-service.model';
 })
 export class OrderServiceComponent implements OnInit {
 
-    public address: string = '';
-    public number: string = '';
-    public complement: string = '';
-    public payWay: string = '';
-
-    public addressValid: boolean;
-    public numberValid: boolean;
-    public complementValid: boolean;
-    public payWayValid: boolean;
-
-    public addressPristine: boolean = true;
-    public numberPristine: boolean = true;
-    public complementPristine: boolean = true;
-    public payWayPristine: boolean = true;
-
-    public formValid: string = 'disabled';
-    private idOrderService: number;
+    @ViewChild('myForm') public form: NgForm
 
     constructor(private orderServiceService: OrderServiceService) { }
 
-    ngOnInit() {
-    }
+    ngOnInit() {}
 
-    updateAddress(address: string): void {
-        this.address = address;
-        this.addressPristine = false;
-        this.addressValid = (this.address.length > 3) ? true : false;
-        this.enabledForm();
-    }
-
-    updateNumber(number: string): void {
-        this.number = number;
-        this.numberPristine = false;
-        this.numberValid = (this.number.length > 0) ? true : false;
-        this.enabledForm();
-    }
-
-    updateComplement(complement: string): void {
-        this.complement = complement;
-        this.complementPristine = false
-        this.complementValid = (this.complement.length > 3) ? true : false;
-        this.enabledForm();
-    }
-
-    updatePayWay(payWay: string): void {
-        this.payWay = payWay;
-        this.payWayPristine = false;
-        this.payWayValid = (this.payWay.length > 0) ? true : false;
-        this.enabledForm();
-    }
-
-    enabledForm(): void {
-        this.formValid =
-        ( this.addressValid && this.numberValid && this.payWayValid ) ? '' : 'disabled';
-    }
-
-    confirmBuy(): void {
-
-        let orderService = new OrderService(
-          this.address,
-          this.number,
-          this.complement,
-          this.payWay);
-
-        this.orderServiceService.buy(orderService)
-          .subscribe((idOrderService: number) =>{
-              console.log(idOrderService)
-            this.idOrderService = idOrderService;
-          });
+    public confirmBuy(): void {
+        console.log(this.form);
     }
 
 }
